f9820432d6698965da6172831d1d51cb0a87d0b7
Luis E. Lopez <ux@lelopez.io>
2025-12-06 12:32:15 -0700

    perf: optimized solution and code

--- day-1/part-1/solution.ts
+++ day-1/part-1/solution.ts
@@ -1,42 +1,72 @@
-const file = Bun.file(`${import.meta.dir}/input`)
-const text = await file.text()
-const lines = text.split('\n')
+const FILE = Bun.file(`${import.meta.dir}/input`)
+const TEXT = await FILE.text()
+const LINES = TEXT.split('\n')
 
 const STARTING_POINT = 50
 
-let currentPosition = STARTING_POINT
-let solution = 0
-for (const line of lines) {
-    const [op, val] = [line[0], line.substring(1)]
+const part1SolutionInitial = () => {
+    let currentPosition = STARTING_POINT
+    let solution = 0
+    for (const line of LINES) {
+        const [op, val] = [line[0], line.substring(1)]
 
-    if (!op) {
-        continue
+        if (!op) {
+            continue
+        }
+
+        let endPosition =
+            currentPosition + parseInt(`${op == 'L' ? '-' : '+'}${val}`)
+
+        let remainder = 0
+
+        if (endPosition > 99) {
+            remainder = endPosition % 100
+            endPosition = remainder
+        }
+
+        if (endPosition < 0) {
+            remainder = Math.abs(endPosition) % 100
+            endPosition = remainder === 0 ? 0 : 100 - remainder
+        }
+
+        if (endPosition == 0) {
+            solution++
+        }
+
+        process.env.DEBUG &&
+            console.debug(
+                `currentPosition: ${currentPosition}, ${op} to ${val}, endPosition: ${endPosition}`
+            )
+
+        currentPosition = endPosition
     }
 
-    let endPosition =
-        currentPosition + parseInt(`${op == 'L' ? '-' : '+'}${val}`)
-
-    let remainder = 0
-
-    if (endPosition > 99) {
-        remainder = endPosition % 100
-        endPosition = remainder
-    }
-
-    if (endPosition < 0) {
-        remainder = Math.abs(endPosition) % 100
-        endPosition = remainder === 0 ? 0 : 100 - remainder
-    }
-
-    if (endPosition == 0) {
-        solution++
-    }
-
-    console.log(
-        `currentPosition: ${currentPosition}, ${op} to ${val}, endPosition: ${endPosition}`
-    )
-
-    currentPosition = endPosition
+    return solution
 }
 
-console.log(`solution: ${solution}`)
+console.log(`part1SolutionInitial:\t${part1SolutionInitial()}`)
+
+const part1SolutionOptimized = () => {
+    let currentPosition = STARTING_POINT
+    let solution = 0
+
+    for (const line of LINES) {
+        if (!line) continue
+
+        // handle turns greater than 100 clicks (full turns)
+        const delta = (line[0] === 'L' ? -1 : 1) * parseInt(line.substring(1))
+        const effectiveDelta = delta % 100
+
+        // handle converting the change back to valid position (in range)
+        currentPosition += effectiveDelta
+        if (currentPosition >= 100) currentPosition -= 100
+        if (currentPosition < 0) currentPosition += 100
+
+        // check if zero
+        if (currentPosition === 0) solution++
+    }
+
+    return solution
+}
+
+console.log(`part1SolutionOptimized:\t${part1SolutionOptimized()}`)
