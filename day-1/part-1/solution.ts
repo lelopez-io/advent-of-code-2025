bb1017d9c733194849c6593d47ac225388a8620e
Luis E. Lopez <ux@lelopez.io>
2025-12-06 11:29:37 -0700

    wip: init part 1

--- day-1/part-1/solution.ts
+++ day-1/part-1/solution.ts
@@ -1,4 +1,42 @@
 const file = Bun.file(`${import.meta.dir}/input`)
 const text = await file.text()
+const lines = text.split('\n')
 
-console.log(text)
+const STARTING_POINT = 50
+
+let currentPosition = STARTING_POINT
+let solution = 0
+for (const line of lines) {
+    const [op, val] = [line[0], line.substring(1)]
+
+    if (!op) {
+        continue
+    }
+
+    let endPosition =
+        currentPosition + parseInt(`${op == 'L' ? '-' : '+'}${val}`)
+
+    let remainder = 0
+
+    if (endPosition > 99) {
+        remainder = endPosition % 99
+        endPosition = 0 + remainder
+    }
+
+    if (endPosition < 0) {
+        remainder = endPosition % 99
+        endPosition = 100 + remainder
+    }
+
+    if (endPosition == 0) {
+        solution++
+    }
+
+    console.log(
+        `currentPosition: ${currentPosition}, ${op} to ${val}, endPosition: ${endPosition}`
+    )
+
+    currentPosition = endPosition
+}
+
+console.log(`solution: ${solution}`)
