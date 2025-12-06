9022f564bdf665738620f25b0259267491b3bb21
Luis E. Lopez <ux@lelopez.io>
2025-12-06 16:50:00 -0700

    test: refactored for better ergonomics

--- day-1/part-1/solution.test.ts
+++ day-1/part-1/solution.test.ts
@@ -1,4 +1,4 @@
-import { test, expect } from 'bun:test'
+import { test, expect, describe } from 'bun:test'
 import {
     countZeroCrossings,
     part1SolutionInitial,
@@ -12,70 +12,77 @@ const loadInput = async () => {
     return TEXT.split('\n')
 }
 
-test('part1SolutionInitial - works', async () => {
-    expect(part1SolutionInitial(50, await loadInput())).toBe(1129)
+describe('integration tests', () => {
+    test('part1SolutionInitial - works', async () => {
+        const solution = part1SolutionInitial(50, await loadInput())
+        expect(solution).toMatchInlineSnapshot(`1129`)
+    })
+
+    test('part1SolutionOptimized- works', async () => {
+        const solution = part1SolutionOptimized(50, await loadInput())
+        expect(solution).toMatchInlineSnapshot(`1129`)
+    })
+
+    test('part2SolutionInitial - works', async () => {
+        const solution = part2SolutionInitial(50, await loadInput())
+        expect(solution).toMatchInlineSnapshot(`6638`)
+    })
 })
 
-test('part1SolutionOptimized- works', async () => {
-    expect(part1SolutionOptimized(50, await loadInput())).toBe(1129)
-})
+describe('unit tests', () => {
+    test('moving right - reaches zero exactly', () => {
+        expect(countZeroCrossings(50, 50)).toBe(1)
+    })
 
-test('part2SolutionInitial - works', async () => {
-    expect(part2SolutionInitial(50, await loadInput())).toBe(6638)
-})
+    test("moving right - doesn't reach zero", () => {
+        expect(countZeroCrossings(50, 30)).toBe(0)
+    })
 
-test('moving right - reaches zero exactly', () => {
-    expect(countZeroCrossings(50, 50)).toBe(1)
-})
+    test('moving right - crosses zero twice', () => {
+        expect(countZeroCrossings(50, 150)).toBe(2)
+    })
 
-test("moving right - doesn't reach zero", () => {
-    expect(countZeroCrossings(50, 30)).toBe(0)
-})
+    test('moving left - reaches zero exactly', () => {
+        expect(countZeroCrossings(50, -50)).toBe(1)
+    })
 
-test('moving right - crosses zero twic', () => {
-    expect(countZeroCrossings(50, 150)).toBe(2)
-})
+    test("moving left - doesn't reach zero", () => {
+        expect(countZeroCrossings(50, -30)).toBe(0)
+    })
 
-test('moving left - reaches zero exactly', () => {
-    expect(countZeroCrossings(50, -50)).toBe(1)
-})
+    test('moving left - crosses zero twice', () => {
+        expect(countZeroCrossings(50, -150)).toBe(2)
+    })
 
-test("moving left - doesn't reach zero", () => {
-    expect(countZeroCrossings(50, -30)).toBe(0)
-})
+    test('edge-case - start at 99, move right 1', () => {
+        expect(countZeroCrossings(99, 1)).toBe(1)
+    })
 
-test('moving left - crosses zero twice', () => {
-    expect(countZeroCrossings(50, -150)).toBe(2)
-})
+    test('edge-case - start at 1, move left 1', () => {
+        expect(countZeroCrossings(1, -1)).toBe(1)
+    })
 
-test('edge-case - start at 99, move right 1', () => {
-    expect(countZeroCrossings(99, 1)).toBe(1)
-})
+    test('start at 0 - move right small', () => {
+        expect(countZeroCrossings(0, 5)).toBe(0)
+    })
 
-test('edge-case - start at 1, move left 1', () => {
-    expect(countZeroCrossings(1, -1)).toBe(1)
-})
+    test('start at 0 - move left small', () => {
+        expect(countZeroCrossings(0, -5)).toBe(0)
+    })
 
-test('start at 0 - move right small', () => {
-    expect(countZeroCrossings(0, 5)).toBe(0)
-})
+    test('start at 0 - move right full loop', () => {
+        expect(countZeroCrossings(0, 100)).toBe(1)
+    })
 
-test('start at 0 - move left small', () => {
-    expect(countZeroCrossings(0, -5)).toBe(0)
-})
+    test('start at 0 - move left full loop', () => {
+        expect(countZeroCrossings(0, -100)).toBe(1)
+    })
 
-test('start at 0 - move right full loop', () => {
-    expect(countZeroCrossings(0, 100)).toBe(1)
-})
+    test('start at 0 - move right multiple loops', () => {
+        expect(countZeroCrossings(0, 250)).toBe(2)
+    })
 
-test('start at 0 - move left full loop', () => {
-    expect(countZeroCrossings(0, -100)).toBe(1)
-})
-
-test('start at 0 - move right multiple loops', () => {
-    expect(countZeroCrossings(0, 250)).toBe(2)
-})
-
-test('start at 0 - move left multiple loops', () => {
-    expect(countZeroCrossings(0, -250)).toBe(2)
+    test('start at 0 - move left multiple loops', () => {
+        expect(countZeroCrossings(0, -250)).toBe(2)
+    })
 })
