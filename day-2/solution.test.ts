aaa218820f8f27735704a60385b528489a2d9ff1
Luis E. Lopez <ux@lelopez.io>
2025-12-07 11:09:25 -0700

    test: init sample cases

--- day-2/solution.test.ts
+++ day-2/solution.test.ts
@@ -1,5 +1,10 @@
 import { test, expect, describe } from 'bun:test'
-import { part1Initial, part1Optimized, part2Initial } from './solution'
+import {
+    part1Initial,
+    part1Optimized,
+    part2Initial,
+    validateRange,
+} from './solution'
 
 const loadInput = async () => {
     const FILE = Bun.file(`${import.meta.dir}/input`)
@@ -8,22 +13,52 @@ const loadInput = async () => {
 }
 
 describe('integration tests', () => {
-    test('part1Initial - works', async () => {
+    test('part1Initial', async () => {
         const solution = part1Initial(await loadInput())
         expect(solution).toMatchInlineSnapshot(`1129`)
     })
 
-    test('part1Optimized- works', async () => {
+    test('part1Optimized', async () => {
         const solution = part1Optimized(await loadInput())
         expect(solution).toMatchInlineSnapshot(`1129`)
     })
 
-    test('part2Initial - works', async () => {
+    test('part2Initial', async () => {
         const solution = part2Initial(await loadInput())
         expect(solution).toMatchInlineSnapshot(`6638`)
     })
 })
 
-describe('unit tests', () => {
-    test('', () => {})
+describe('unit tests - sample cases', () => {
+    test('validateRange - example1', () => {
+        expect(validateRange(11, 22)).toEqual([11, 22])
+    })
+
+    test('validateRange - example2', () => {
+        expect(validateRange(95, 115)).toEqual([99])
+    })
+
+    test('validateRange - example3', () => {
+        expect(validateRange(998, 1012)).toEqual([1010])
+    })
+
+    test('validateRange - example4', () => {
+        expect(validateRange(1188511880, 1188511890)).toEqual([1188511885])
+    })
+
+    test('validateRange - example5', () => {
+        expect(validateRange(222220, 222224)).toEqual([222222])
+    })
+
+    test('validateRange - example6', () => {
+        expect(validateRange(1698522, 1698528)).toEqual([])
+    })
+
+    test('validateRange - example7', () => {
+        expect(validateRange(446443, 446449)).toEqual([446446])
+    })
+
+    test('validateRange - example8', () => {
+        expect(validateRange(38593856, 38593862)).toEqual([38593859])
+    })
 })
--- day-2/solution.ts
+++ day-2/solution.ts
@@ -1,5 +1,15 @@
+export const validateRange = (rangeStart: number, rangeEnd: number) => {
+    console.log(`${rangeStart} to ${rangeEnd}`)
+
+    return [rangeStart, rangeEnd]
+}
+
 export const part1Initial = (lines: string[]) => {
-    throw Error('not implemented')
+    for (const line of lines) {
+        const [rangeStart, rangeEnd] = line.split('-')
+
+        const invalidIds = validateRange(rangeStart, rangeEnd)
+    }
 }
 
 export const part1Optimized = (lines: string[]) => {
