e6b9f0e458d42d5fedff18d315667e5af76e8c12
Luis E. Lopez <ux@lelopez.io>
2025-12-07 13:53:58 -0700

    refactor: cleanup part 1

--- day-2/part1.ts
+++ day-2/part1.ts
@@ -1,21 +1,28 @@
-import { parseRange } from './parseRange'
+import { sumInvalidIds } from './sumInvalidIds'
+
+const isRepeatingPattern = (id: number): boolean => {
+    const idString = `${id}`
+    const idLength = idString.length
+
+    if (idLength % 2 != 0) {
+        return false
+    }
+
+    if (
+        idString.substring(0, idLength / 2) === idString.substring(idLength / 2)
+    ) {
+        process.env.DEBUG && console.debug(`substring match - pushing ${id}`)
+        return true
+    }
+
+    return false
+}
 
 export const validateRange = (rangeStart: number, rangeEnd: number) => {
     let invalidIds = []
 
     for (let i = rangeStart; i <= rangeEnd; i++) {
-        const idString = `${i}`
-        const idLength = idString.length
-
-        if (idLength % 2 != 0) {
-            continue
-        }
-
-        if (
-            idString.substring(0, idLength / 2) ===
-            idString.substring(idLength / 2)
-        ) {
-            process.env.DEBUG && console.debug(`substring match - pushing ${i}`)
+        if (isRepeatingPattern(i)) {
             invalidIds.push(i)
         }
     }
@@ -24,26 +31,5 @@ export const validateRange = (rangeStart: number, rangeEnd: number) => {
 }
 
 export const part1Initial = (lines: string[]) => {
-    const solution = []
-
-    for (const line of lines) {
-        process.env.DEBUG && console.debug(`line: ${line}`)
-
-        const range = parseRange(line)
-        if (!range) continue
-        process.env.DEBUG && console.debug(`range: ${range}`)
-
-        const invalidIds = validateRange(range[0], range[1])
-
-        if (invalidIds.length) {
-            process.env.DEBUG && console.debug(`invalidIds: ${invalidIds}`)
-            solution.push(invalidIds)
-        }
-    }
-
-    return solution.flat().reduce((acc, n) => acc + n, 0)
-}
-
-export const part1Optimized = (lines: string[]) => {
-    throw Error('not implemented')
+    return sumInvalidIds(lines, validateRange)
 }
