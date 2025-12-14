5e86565456c7711ffac6e54d53f7d2a9de404aab
Luis E. Lopez <ux@lelopez.io>
2025-12-13 19:59:52 -0700

    feat: first unit test

--- src/day/4/part1.test.ts
+++ src/day/4/part1.test.ts
@@ -3,17 +3,18 @@ import { part1Initial, processLine } from './part1'
 import { loadInput } from './loadInput'
 
 describe('part1 - integration', () => {
-    test.each([
-        { fn: part1Initial, name: 'part1Initial' },
-    ])('$name', async ({ fn }) => {
-        const solution = fn(await loadInput())
-        expect(solution).toMatchInlineSnapshot()
-    })
+    test.each([{ fn: part1Initial, name: 'part1Initial' }])(
+        '$name',
+        async ({ fn }) => {
+            const solution = fn(await loadInput())
+            expect(solution).toMatchInlineSnapshot()
+        }
+    )
 })
 
 describe('part1 - unit', () => {
     test.each([
-        // { input: 'example', expected: 42 },
+        { input: [undefined, '..@@.@@@@.', '@@@.@.@.@@'], expected: 5 },
     ])('processLine($input) returns $expected', ({ input, expected }) => {
         expect(processLine(input)).toEqual(expected)
     })
--- src/day/4/part1.ts
+++ src/day/4/part1.ts
@@ -1,5 +1,30 @@
-export const processLine = (line: string): number => {
-    throw new Error('Not implemented')
+export const processLine = (
+    lines: ReadonlyArray<string | undefined>
+): number => {
+    let solution = 0
+
+    for (let i = 0; i < lines[1]!.length; i++) {
+        if (lines[1]![i] !== '@') continue
+
+        const topSlice = (lines[0] ?? '')
+            .substring(i - 1, i + 2)
+            .split('')
+            .filter((c) => c === '@').length
+
+        const bottomSlice = (lines[2] ?? '')
+            .substring(i - 1, i + 2)
+            .split('')
+            .filter((c) => c === '@').length
+
+        const left = lines[1]![i - 1] == '@' ? 1 : 0
+        const right = lines[1]![i + 1] == '@' ? 1 : 0
+
+        if (topSlice + bottomSlice + left + right < 4) {
+            solution++
+        }
+    }
+
+    return solution
 }
 
 export const part1Initial = (lines: string[]): number => {
