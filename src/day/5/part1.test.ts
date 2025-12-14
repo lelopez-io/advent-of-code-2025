2b136b1cedf4b0eefccf3a5780a59f0ab78a1752
Luis E. Lopez <ux@lelopez.io>
2025-12-13 17:23:25 -0700

    chore: init day 5

---
+++ src/day/5/part1.test.ts
@@ -0,0 +1,20 @@
+import { test, expect, describe } from 'bun:test'
+import { part1Initial, processLine } from './part1'
+import { loadInput } from './loadInput'
+
+describe('part1 - integration', () => {
+    test.each([
+        { fn: part1Initial, name: 'part1Initial' },
+    ])('$name', async ({ fn }) => {
+        const solution = fn(await loadInput())
+        expect(solution).toMatchInlineSnapshot()
+    })
+})
+
+describe('part1 - unit', () => {
+    test.each([
+        // { input: 'example', expected: 42 },
+    ])('processLine($input) returns $expected', ({ input, expected }) => {
+        expect(processLine(input)).toEqual(expected)
+    })
+})
---
+++ src/day/5/part2.ts
@@ -0,0 +1,7 @@
+export const processLine = (line: string): number => {
+    throw new Error('Not implemented')
+}
+
+export const part2Initial = (lines: string[]): number => {
+    throw new Error('Not implemented')
+}
